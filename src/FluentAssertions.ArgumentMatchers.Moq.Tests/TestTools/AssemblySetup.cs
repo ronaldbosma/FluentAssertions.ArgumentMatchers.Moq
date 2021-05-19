77a34c949d0c3a997ac83e6682b21f2fb6c100ce
Ronald Bosma <ronald.bosma@infosupport.com>
2021-05-19 17:02:23 +0200

    Add assembly setup to print assembly versions

---
+++ src/FluentAssertions.ArgumentMatchers.Moq.Tests/TestTools/AssemblySetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FluentAssertions.ArgumentMatchers.Moq.Tests.TestTools
+{
+    [TestClass]
+    public class AssemblySetup
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            // Print the FluentAssertions.ArgumentMatchers.FakeItEasy, FakeItEasy and FluentAssertions assembly version
+            // so we can check which versions are used during the tests.
+            // The 'tests' folder in the repository contains unit tests that reuse the tests of this project
+            // and execute tests with different version of FakeItEasy and FluentAssertions
+            PrintAssemblyVersion(typeof(Its).Assembly);
+            PrintAssemblyVersion(typeof(Mock).Assembly);
+            PrintAssemblyVersion(typeof(TypeExtensions).Assembly);
+        }
+
+        private static void PrintAssemblyVersion(Assembly assembly)
+        {
+            var assemblyName = assembly.GetName();
+            var assemblyFileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            Trace.WriteLine($"{assemblyName.Name} - {assemblyName.Version} - {assemblyFileVersionInfo.FileVersion} - {assemblyFileVersionInfo.ProductVersion}");
+        }
+    }
+}
